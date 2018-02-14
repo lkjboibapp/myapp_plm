7d2cd09b7f67e643cee2d2f2a8c1d851fdbbec06
kuhasun <kuhasun@live.com>
2018-02-14 23:46:51 +0700

    แก้บัคหน้า วีดีโอ

--- src/pages/vdo/vdo.ts
+++ src/pages/vdo/vdo.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
-import { HttpClient } from '@angular/common/http';
 
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/timeout';
@@ -12,28 +11,10 @@ import 'rxjs/add/operator/timeout';
 export class VdoPage {
   public news: any; // กำหนดตัวแปร สำหรับรับค่าข้อมูลจาก api
 
-  constructor(public navCtrl: NavController,
-    public navParams: NavParams,
-    public http: HttpClient // inject HttpClient เข้ามาใช้งาน
-  ) {
-    this.ETPhoneHome();
-    console.log('======================================');
+  constructor(  public navCtrl: NavController,
+                public navParams: NavParams
+              ) {
+
   }
 
-  ETPhoneHome() {
-    let path = 'http://localhost:80/ServiceMobile/Service_VDO_Usability_Report_New/ServiceVDO.php/getvdo';
-    let encodedPath = encodeURI(path);
-    console.log(encodedPath)
-    let timeoutMS = 100;
-
-    this.http.get(encodedPath)
-      .timeout(timeoutMS)
-      .map(res => res.json()).subscribe(data => {
-        this.news = data.data;
-        console.log(data)
-      },
-        err => {
-          console.log("ldokvopdsv");
-        });
-      }
 }

48d488af1b3826f1ce6e807246a4978cc04192cd
kuhasun <kuhasun@live.com>
2018-02-21 11:20:00 +0700

    vdo create

--- src/pages/vdo/vdo.ts
+++ src/pages/vdo/vdo.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { NavController, AlertController } from 'ionic-angular';
+import { Http } from '@angular/http';
 
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/timeout';
@@ -9,12 +10,66 @@ import 'rxjs/add/operator/timeout';
   templateUrl: 'vdo.html',
 })
 export class VdoPage {
-  public news: any; // กำหนดตัวแปร สำหรับรับค่าข้อมูลจาก api
+  public results_filter: any;
+  public results: any;
+  public text: any;
 
-  constructor(  public navCtrl: NavController,
-                public navParams: NavParams
-              ) {
+  constructor(public http: Http, public navCtrl: NavController, private alertCtrl: AlertController) {
+    this.ETPhoneHome();
 
+
+    console.log("123", this.ETPhoneHome());
   }
 
+  ETPhoneHome() {
+    let path = 'http://localhost:80/Service/ServiceMobile/ServiceVDO.php/getvdo';
+    let encodedPath = encodeURI(path);
+    // console.log(encodedPath)
+    let timeoutMS = 100;
+
+    this.http.get(encodedPath)
+      .timeout(timeoutMS)
+      .map(res => res.json()).subscribe(data => {
+        this.results = data.data;
+        // console.log(data)
+      },
+        err => {
+          console.log("ldokvopdsv");
+        });
+  }
+
+
+  getItems(ev: any) {
+    let val = ev.target.value;
+
+    if (val && val.trim() != '') {
+      this.results = this.results.filter((item) => {
+
+        // ในที่นี้เราค้นหาจาก name ของ item ก็กำหนด item.name ซึ่งเป็นชื่อจังหวัด
+        // return (item.vdo_title.indexOf(val) > -1);
+        return (item.vdo_title.toLowerCase().indexOf(val.toLowerCase()) > -1);
+      });
+    } else if (val == '') {
+      this.ETPhoneHome();
+    }
+  }
+
+  openItem(item) {
+    let text = item.vdo_id;
+
+    this.text = this.results.filter((item) => {
+
+      // ในที่นี้เราค้นหาจาก name ของ item ก็กำหนด item.name ซึ่งเป็นชื่อจังหวัด
+      // return (item.vdo_title.indexOf(val) > -1);
+      return (item.vdo_id.toLowerCase().indexOf(text.toLowerCase()) > -1);
+    });
+
+    console.log("show text =", this.text);
+    //   let alert = this.alertCtrl.create({
+    //     title:'alert',
+    //     subTitle: item.vdo_title,
+    //     buttons: ['OK']
+    //   });
+    //   alert.present();
+  }
 }

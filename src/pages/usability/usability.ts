327f2e622d97995fe85d69fc0635f578bff5f83f
kuhasun <kuhasun@live.com>
2018-03-08 14:17:19 +0700

    usa update

--- src/pages/usability/usability.ts
+++ src/pages/usability/usability.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController,AlertController } from 'ionic-angular';
+import { NavController, AlertController } from 'ionic-angular';
 import { Http } from '@angular/http';
 
 
@@ -11,69 +11,66 @@ import 'rxjs/add/operator/timeout';
 })
 export class UsabilityPage {
   public usability: any;
-  public items_filter : any ;
-  items :any;
-  alert:any;
- 
-  constructor(public navCtrl: NavController,public alerCtrl: AlertController, public http: Http) {
+  public items_filter: any;
+  items: any;
+  alert: any;
+
+  constructor(public navCtrl: NavController, public alerCtrl: AlertController, public http: Http) {
+}
+  ngAfterViewInit() {
     this.ETPhoneHome();
 
-       console.log('======================================');
+  }
 
+  ETPhoneHome() {
+    let path = 'http://localhost/Service/ServiceMobile/Serviceusability.php/usability';
+    let encodedPath = encodeURI(path);
 
-    }
-    
-    ETPhoneHome() {
-      let path = 'http://localhost/Service/ServiceMobile/Serviceusability.php/usability';
-        let encodedPath = encodeURI(path);
-        let timeoutMS = 100;
-
-        this.http.get(encodedPath)
-          .timeout(timeoutMS)
-          .map(res => res.json()).subscribe(data => {
-              let responseData = data;
-              this.usability = responseData.data;
-              // console.log("show data = ",responseData.data);
-              console.log("ETPhoneHome",this.usability);
-          },
-              err => {
-                  console.log('error in ETPhoneHome');
-              });
-            }     
-            
-
-            getItems(ev:any) {
-                // Reset items back to all of the items
-                // set val to the value of the searchbar
-                let val = ev.target.value;
-                
-                // if the value is an empty string don't filter the items
-                if (val && val.trim() != '') {
-                    val = val.toLowerCase();
-                    console.log(val)
-
-                    // console.log("test usa", this.usability);
-                    this.usability = this.usability .filter((item) => {
-                        // console.log(item.usa_title.toLowerCase())
-                        return (item.usa_title.toLowerCase().indexOf(val.toLowerCase()) > -1);
-                      });
-                  }else if (val =='') {
-                      this.ETPhoneHome();
-        }
-    }
-    
-      doAlert(item) {
-        let alert = this.alerCtrl.create({
-          title: item.usa_title,
-          message: item.usa_detail,
-          buttons: ['Ok']
+    this.http.get(encodedPath)
+      .map(res => res.json()).subscribe(data => {
+        let responseData = data;
+        this.usability = responseData.data;
+        // console.log("show data = ",responseData.data);
+        console.log("ETPhoneHome", this.usability);
+      },
+        err => {
+          console.log('error in ETPhoneHome');
         });
-        alert.present()
-      }
- }
- 
- 
- 
-   
+  }
+
+
+  getItems(ev: any) {
+    // Reset items back to all of the items
+    // set val to the value of the searchbar
+    let val = ev.target.value;
+
+    // if the value is an empty string don't filter the items
+    if (val && val.trim() != '') {
+      val = val.toLowerCase();
+      console.log(val)
+
+      // console.log("test usa", this.usability);
+      this.usability = this.usability.filter((item) => {
+        // console.log(item.usa_title.toLowerCase())
+        return (item.usa_title.toLowerCase().indexOf(val.toLowerCase()) > -1);
+      });
+    } else if (val == '') {
+      this.ETPhoneHome();
+    }
+  }
+
+  doAlert(item) {
+    let alert = this.alerCtrl.create({
+      title: item.usa_title,
+      message: item.usa_detail,
+      buttons: ['Ok']
+    });
+    alert.present()
+  }
+}
+
+
+
+
 
 

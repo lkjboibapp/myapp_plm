4926057c8a6c74a8650a0893876aba449c78d40a
siripat1234 <siripats57@email.nu.ac.th>
2018-03-12 11:05:30 +0700

    ammee

--- src/pages/documents/documents.ts
+++ src/pages/documents/documents.ts
@@ -1,13 +1,67 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController,AlertController } from 'ionic-angular';
+import { Http } from '@angular/http';
+
+//import { FileTransfer, FileUploadOptions, FileTransferObject } from '@ionic-native/file-transfer';
+//import { File } from '@ionic-native/file';
+
 
 @Component({
   selector: 'page-documents',
   templateUrl: 'documents.html'
 })
 export class DocumentsPage {
-
-  constructor(public navCtrl: NavController) {
+  public download: any;
+  public items_filter : any ;
+  items :any;
+ 
+ 
+  constructor(public navCtrl: NavController,public alerCtrl: AlertController, public http: Http) {
   }
-  
-}
+    ngAfterViewInit(){
+      this.ETPhoneHome();
+    }
+    ETPhoneHome() {
+      let path = 'http://localhost/Service/ServiceMobile/ServiceDoc.php/getdocs';
+        let encodedPath = encodeURI(path);
+
+        this.http.get(encodedPath)
+          .map(res => res.json()).subscribe(data => {
+              let responseData = data;
+              this.download = responseData.data;
+              console.log("show data = ",this.download);
+          },
+              err => {
+                  console.log('error in ETPhoneHome');
+              });
+            }     
+            
+
+            getItems(ev:any) {
+                // Reset items back to all of the items
+                // set val to the value of the searchbar
+                let val = ev.target.value;
+                
+                // if the value is an empty string don't filter the items
+                if (val && val.trim() != '') {
+                    val = val.toLowerCase();
+                    console.log(val)
+
+                    // console.log("test usa", this.usability);
+                    this.download = this.download .filter((item) => {
+                        // console.log(item.usa_title.toLowerCase())
+                        return (item.dty_name.toLowerCase().indexOf(val.toLowerCase()) > -1);
+                      });
+                  }else if (val =='') {
+                      this.ETPhoneHome();
+             }
+        
+      }
+     
+      doAlert(item) {
+        let alert = this.alerCtrl.create({
+          buttons: ['Ok']
+        });
+        alert.present()
+      }
+    }

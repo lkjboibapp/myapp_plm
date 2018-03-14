21695b77edc6c897a92e47b6464665bb4c1147e7
nutsuckseed <anas-dobimor@outlook.com>
2018-03-14 11:48:24 +0700

    newcontact

--- src/pages/contact/contact.ts
+++ src/pages/contact/contact.ts
@@ -2,14 +2,26 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { Http } from '@angular/http';
 import{EditprofilePage} from '../editprofile/editprofile';
+import {LoginPage} from '../login/login';
 
 @Component({
   selector: 'page-contact',
   templateUrl: 'contact.html'
 })
 export class ContactPage {
+  
+  userDetails:any;
 
-  constructor(public navCtrl: NavController) {}
+  constructor(public navCtrl: NavController) {
+    const data = JSON.parse(localStorage.getItem('userData'));
+    this.userDetails = data;
+    console.log(this.userDetails)
+  }
+  logoutPage(){
+    // this.userDetails.clearCache();
+    this.userDetails.clearAll();
+    this.navCtrl.setRoot(LoginPage);
+    }
   editprofilePage(){
      this.navCtrl.push(EditprofilePage);
   }

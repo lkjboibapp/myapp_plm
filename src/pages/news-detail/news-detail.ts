69db2078f762dcd1ecb45da9d2b6c642bba59d2e
siripat1234 <siripats57@email.nu.ac.th>
2018-03-15 14:49:48 +0700

    ammee

--- src/pages/news-detail/news-detail.ts
+++ src/pages/news-detail/news-detail.ts
@@ -12,7 +12,9 @@ export class NewsDetailPage {
 
   constructor(public navCtrl: NavController) {
     const data = JSON.parse(localStorage.getItem('itemNew'));
+   
     this.cms_title = data['cms_title'];
+    this.cms_detail = data['cms_detail'];
     this.cms_short_title = data['cms_short_title'];
     this.create_date = data['create_date'];
   }

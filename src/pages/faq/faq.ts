279ff667165856abb043393577208f043229525b
kuhasun <kuhasun@live.com>
2018-03-19 11:33:47 +0700

    faq

--- src/pages/faq/faq.ts
+++ src/pages/faq/faq.ts
@@ -17,6 +17,9 @@ export class FaqPage {
        this.ETPhoneHomeQuery();
         // console.log('คือ======================================');
     }
+
+   
+
     ETPhoneHome() {
         let path = 'http://localhost/Service/ServiceMobile/ServiceFAQ.php/getfaq_type';
         let encodedPath = encodeURI(path);
@@ -25,7 +28,6 @@ export class FaqPage {
             .timeout(timeoutMS)
             .map(res => res.json()).subscribe(dataf => {
                 this.faq = dataf.data;
-                // console.log("TestETPhoneHome8");
             },
                 err => {
                     console.log("Error");
@@ -34,41 +36,35 @@ export class FaqPage {
 
     ETPhoneHomeQuery() {
       let pathh = 'http://localhost/Service/ServiceMobile/ServiceFAQ.php/getfaq';
-      console.log("TestETPhoneHome2");
       let encodedPathh = encodeURI(pathh);
-      console.log("TestETPhoneHome3");
       let timeoutMSS = 10000;
-      console.log("TestETPhoneHome4");
       this.http.get(encodedPathh)
           .timeout(timeoutMSS)
           .map(ress => ress.json()).subscribe(dataa => {
-            console.log("TestETPhoneHome5");
               this.faqq = dataa.data;
-              console.log("this.wwww คือ "+this.faqq);
           },
           err => {
               console.log("Error");
           });
     }
 
+    ngAfterViewInit() {
+    }
+
     getItems(ev: any) {
-        console.log("คือ getItems ก่อน")
         let val = ev.target.value;
         if (val && val.trim() != '') {
-            console.log("คือgetItems if ก่อน")
             this.faq = this.faq.filter((item) => {
                 return (item.faq_type_title_TH.toLowerCase().indexOf(val.toLowerCase()) > -1);
             });
         } else if (val == '') {
-            console.log("คือgetItems else if ก่อน")
             this.ETPhoneHome();
         }
     }
 
     toggleSection(i) {//iคือ Array
         this.faq[i].open = !this.faq[i].open;
-        console.log("คือ toggleSection i"+i)
-        console.log("คือ toggleSection this.faq[i]"+this.faq[i])
+        // console.log("คือ toggleSection this.faq[i]"+this.faq[i])
 
     }
 }

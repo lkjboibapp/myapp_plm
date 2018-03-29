279d4f425fc1e6afe84dd80b00241c88d0393051
kuhasun <kuhasun@live.com>
2018-03-29 17:40:06 +0700

    sun

--- src/pages/search/search.ts
+++ src/pages/search/search.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
+import { AuthServiceProvider } from '../../providers/auth-service/auth-service';
 
 /**
  * Generated class for the SearchPage page.
@@ -16,12 +17,38 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class SearchPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  public banner: any;
+  //==========
+  public banners: any;
+  ///++++++++++++++++
+  data: any;
+
+  constructor(public authService: AuthServiceProvider, public navCtrl: NavController, public navParams: NavParams) {
+    this.bannerService();
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad SearchPage');
   }
 
+  onInput(ev: any) {
+    let val = ev.target.value;
+    console.log("val->", val);
+    if (val && val.trim() != '') {
+      this.banners = this.banner.filter((item) => {
+        return (item.imgslide_title.toLowerCase().indexOf(val.toLowerCase()) > -1);
+      });
+    } else if (val == '') {
+      this.bannerService();
+    }
+  }
+
+  bannerService() {
+    this.authService.bannerGetData(this.data, 'getImgslide').then((result) => {
+      this.banner = result['data'];
+    }, (err) => {
+      console.log(err);
+    });
+  }
 
 }
